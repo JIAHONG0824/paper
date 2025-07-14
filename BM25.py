2d66b0c6c572d0b0f1beb449674d7016601ca997
jiahong <0701phantom@gmail.com>
2025-07-14 11:08:24 +0800

    feat: Implement BM25 for BEIR/dataset baseline reproduction

--- BM25.py
+++ BM25.py
@@ -23,12 +23,15 @@ if __name__ == "__main__":
 
     topics = get_topics(f"beir-v1.0.0-{args.dataset}-test")
     searcher = LuceneSearcher.from_prebuilt_index(f"beir-v1.0.0-{args.dataset}.flat")
+
     queries = []
     qids = []
     for qid in topics:
         queries.append(topics[qid]["title"])
         qids.append(str(qid))
+
     results = searcher.batch_search(queries=queries, qids=qids, k=100, threads=32)
+
     with open(f"run.beir.bm25-flat.{args.dataset}.txt", "w") as f:
         for qid, hits in tqdm(results.items()):
             hits = [hit for hit in hits if hit.docid != qid]
@@ -42,6 +45,7 @@ if __name__ == "__main__":
         text=True,
     )
     ndcg_cut_10 = float(result.stdout.split()[2])
+
     result = subprocess.run(
         f"python -m pyserini.eval.trec_eval -c -m recall.100 beir-v1.0.0-{args.dataset}-test run.beir.bm25-flat.{args.dataset}.txt",
         shell=True,
@@ -49,5 +53,6 @@ if __name__ == "__main__":
         text=True,
     )
     recall_100 = float(result.stdout.split()[2])
+
     print(f"Dataset: {args.dataset}")
     print(f"nDCG@10: {ndcg_cut_10:.3f}\nRecall@100: {recall_100:.3f}")
