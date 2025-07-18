ee76435b6a061a747032e5a51cf72c58f5f94a0e
jiahong <0701phantom@gmail.com>
2025-07-18 12:34:59 +0800

    feat: Add parameters --k --ndcg_k --recall_k

--- Contriever.py
+++ Contriever.py
@@ -18,6 +18,24 @@ if __name__ == "__main__":
         required=True,
         choices=["trec-covid", "trec-news", "scifact"],
     )
+    parser.add_argument(
+        "--k",
+        type=int,
+        default=100,
+        help="Number of top documents to retrieve for each query",
+    )
+    parser.add_argument(
+        "--ndcg_k",
+        type=int,
+        default=10,
+        help="Depth for nDCG calculation (default: 10)",
+    )
+    parser.add_argument(
+        "--recall_k",
+        type=int,
+        default=100,
+        help="Depth for recall calculation (default: 100)",
+    )
     args = parser.parse_args()
 
     if args.model == "facebook/contriever":
@@ -37,7 +55,7 @@ if __name__ == "__main__":
         queries.append(topics[qid]["title"])
         qids.append(str(qid))
 
-    results = searcher.batch_search(queries=queries, q_ids=qids, k=100, threads=16)
+    results = searcher.batch_search(queries=queries, q_ids=qids, k=args.k, threads=16)
 
     with open(f"run.beir.{_model}.{args.dataset}.txt", "w") as f:
         for qid, hits in results.items():
@@ -46,20 +64,20 @@ if __name__ == "__main__":
                 f.write(f"{qid} Q0 {hit.docid} {i + 1} {hit.score} Faiss\n")
 
     result = subprocess.run(
-        f"python -m pyserini.eval.trec_eval   -c -m ndcg_cut.10 beir-v1.0.0-{args.dataset}-test   run.beir.{_model}.{args.dataset}.txt",
+        f"python -m pyserini.eval.trec_eval   -c -m ndcg_cut.{args.ndcg_k} beir-v1.0.0-{args.dataset}-test   run.beir.{_model}.{args.dataset}.txt",
         shell=True,
         capture_output=True,
         text=True,
     )
-    ndcg_cut_10 = float(result.stdout.split()[2])
+    ndcg_cut_10 = float(result.stdout.split()[2]) * 100
 
     result = subprocess.run(
-        f"python -m pyserini.eval.trec_eval   -c -m recall.100 beir-v1.0.0-{args.dataset}-test   run.beir.{_model}.{args.dataset}.txt",
+        f"python -m pyserini.eval.trec_eval   -c -m recall.{args.recall_k} beir-v1.0.0-{args.dataset}-test   run.beir.{_model}.{args.dataset}.txt",
         shell=True,
         capture_output=True,
         text=True,
     )
-    recall_100 = float(result.stdout.split()[2])
+    recall_100 = float(result.stdout.split()[2]) * 100
 
     print(f"Dataset: {args.dataset}")
-    print(f"nDCG@10: {ndcg_cut_10:.3f}\nRecall@100: {recall_100:.3f}")
+    print(f"nDCG@{args.ndcg_k}: {ndcg_cut_10:.1f}\nR@{args.recall_k}: {recall_100:.1f}")
