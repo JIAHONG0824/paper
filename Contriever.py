b4eed9b61df1bf90ca6a3758f7444d691703ab1f
jiahong <0701phantom@gmail.com>
2025-08-01 20:47:51 +0800

    feat:Updata parameters --top_k, --ndcg_cutoff, --recall_cutoffs

--- Contriever.py
+++ Contriever.py
@@ -1,6 +1,7 @@
 from pyserini.search.faiss import FaissSearcher
 from pyserini.encode import AutoQueryEncoder
 from pyserini.search import get_topics
+from tqdm import tqdm
 import subprocess
 import argparse
 
@@ -19,22 +20,15 @@ if __name__ == "__main__":
         choices=["trec-covid", "nfcorpus", "fiqa", "scifact"],
     )
     parser.add_argument(
-        "--k",
-        type=int,
-        default=100,
-        help="Number of top documents to retrieve for each query",
+        "--top_k", type=int, default=100, help="Number of hits to return."
     )
+    parser.add_argument("--ndcg_cutoff", type=int, default=10, help="nDCG@k")
     parser.add_argument(
-        "--ndcg_k",
+        "--recall_cutoffs",
+        nargs=2,
         type=int,
-        default=10,
-        help="Depth for nDCG calculation (default: 10)",
-    )
-    parser.add_argument(
-        "--recall_k",
-        type=int,
-        default=100,
-        help="Depth for recall calculation (default: 100)",
+        default=[100, 1000],
+        help="Recall@k1 and Recall@k2 cutoffs.",
     )
     args = parser.parse_args()
 
@@ -50,34 +44,27 @@ if __name__ == "__main__":
     )
 
     queries = []
-    qids = []
+    q_ids = []
     for qid in topics:
+        q_ids.append(str(qid))
         queries.append(topics[qid]["title"])
-        qids.append(str(qid))
 
-    results = searcher.batch_search(queries=queries, q_ids=qids, k=args.k, threads=16)
+    results = searcher.batch_search(
+        queries=queries, q_ids=q_ids, k=args.top_k, threads=16
+    )
 
     with open(f"run.beir.{_model}.{args.dataset}.txt", "w") as f:
-        for qid, hits in results.items():
+        for qid, hits in tqdm(results.items()):
             hits = [hit for hit in hits if hit.docid != qid]
-            for i, hit in enumerate(hits):
-                f.write(f"{qid} Q0 {hit.docid} {i + 1} {hit.score} Faiss\n")
+            for rank, hit in enumerate(hits):
+                f.write(f"{qid} Q0 {hit.docid} {rank+1} {hit.score} Faiss\n")
 
-    result = subprocess.run(
-        f"python -m pyserini.eval.trec_eval   -c -m ndcg_cut.{args.ndcg_k} beir-v1.0.0-{args.dataset}-test   run.beir.{_model}.{args.dataset}.txt",
-        shell=True,
-        capture_output=True,
-        text=True,
-    )
-    ndcg_cut_10 = float(result.stdout.split()[2]) * 100
-
-    result = subprocess.run(
-        f"python -m pyserini.eval.trec_eval   -c -m recall.{args.recall_k} beir-v1.0.0-{args.dataset}-test   run.beir.{_model}.{args.dataset}.txt",
-        shell=True,
-        capture_output=True,
-        text=True,
-    )
-    recall_100 = float(result.stdout.split()[2]) * 100
-
-    print(f"Dataset: {args.dataset}")
-    print(f"nDCG@{args.ndcg_k}: {ndcg_cut_10:.1f}\nR@{args.recall_k}: {recall_100:.1f}")
+    commands = [
+        f"python -m pyserini.eval.trec_eval -c -m ndcg_cut.{args.ndcg_cutoff} beir-v1.0.0-{args.dataset}-test run.beir.{_model}.{args.dataset}.txt",
+        f"python -m pyserini.eval.trec_eval -c -m recall.{args.recall_cutoffs[0]} beir-v1.0.0-{args.dataset}-test run.beir.{_model}.{args.dataset}.txt",
+        f"python -m pyserini.eval.trec_eval -c -m recall.{args.recall_cutoffs[1]} beir-v1.0.0-{args.dataset}-test run.beir.{_model}.{args.dataset}.txt",
+    ]
+    for command in commands:
+        print(
+            subprocess.run(command, shell=True, capture_output=True, text=True).stdout
+        )
