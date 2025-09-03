1c5049cdc454d8df02296ea920d115a119ba5b8d
jiahong <0701phantom@gmail.com>
2025-09-03 20:49:55 +0800

    新增encoder BAAI/bge-base-en-v1.5

--- Contriever.py
+++ Contriever.py
@@ -1,27 +1,30 @@
 from pyserini.search.faiss import FaissSearcher
 from pyserini.encode import AutoQueryEncoder
-from pyserini.search import get_topics
 from tqdm import tqdm
+import ir_datasets
 import subprocess
 import argparse
-import time
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument(
-        "--model",
+        "--encoder",
         type=str,
         required=True,
-        choices=["facebook/contriever", "facebook/contriever-msmarco"],
+        choices=[
+            "facebook/contriever",
+            "facebook/contriever-msmarco",
+            "BAAI/bge-base-en-v1.5",
+        ],
     )
     parser.add_argument(
         "--dataset",
         type=str,
         required=True,
-        choices=["trec-covid", "nq", "fiqa", "scifact"],
+        choices=["fiqa", "scifact"],
     )
     parser.add_argument(
-        "--top_k", type=int, default=100, help="Number of hits to return."
+        "--top_k", type=int, default=1000, help="Number of hits to return."
     )
     parser.add_argument("--ndcg_cutoff", type=int, default=10, help="nDCG@k")
     parser.add_argument(
@@ -36,34 +39,46 @@ if __name__ == "__main__":
     model_map = {
         "facebook/contriever": "contriever",
         "facebook/contriever-msmarco": "contriever-msmarco",
+        "BAAI/bge-base-en-v1.5": "bge-base-en-v1.5",
     }
-
-    topics = get_topics(f"beir-v1.0.0-{args.dataset}-test")
-    encoder = AutoQueryEncoder(encoder_dir=args.model, pooling="mean", device="cuda:1")
+    if "contriever" in args.encoder:
+        encoder = AutoQueryEncoder(
+            encoder_dir=args.encoder, device="cuda:1", pooling="mean", l2_norm=False
+        )
+    else:
+        encoder = AutoQueryEncoder(
+            encoder_dir=args.encoder,
+            device="cuda:1",
+            pooling="cls",
+            l2_norm=True,
+            prefix="Represent this sentence for searching relevant passages:",
+        )
     searcher = FaissSearcher.from_prebuilt_index(
-        f"beir-v1.0.0-{args.dataset}.{model_map[args.model]}", query_encoder=encoder
+        f"beir-v1.0.0-{args.dataset}.{model_map[args.encoder]}", query_encoder=encoder
     )
+
+    dataset = ir_datasets.load(f"beir/{args.dataset}/test")
 
     queries = []
     q_ids = []
-    for qid in topics:
-        q_ids.append(str(qid))
-        queries.append(topics[qid]["title"])
+    for query in dataset.queries_iter():
+        queries.append(query.text)
+        q_ids.append(query.query_id)
 
     results = searcher.batch_search(
-        queries=queries, q_ids=q_ids, k=args.top_k, threads=16
+        queries=queries, q_ids=q_ids, k=args.top_k, threads=8
     )
 
-    with open(f"run.beir.{model_map[args.model]}.{args.dataset}.txt", "w") as f:
+    with open(f"run.beir.{model_map[args.encoder]}.{args.dataset}.txt", "w") as f:
         for qid, hits in tqdm(results.items()):
             hits = [hit for hit in hits if hit.docid != qid]
             for rank, hit in enumerate(hits):
                 f.write(f"{qid} Q0 {hit.docid} {rank+1} {hit.score} Faiss\n")
 
     commands = [
-        f"python -m pyserini.eval.trec_eval -c -m ndcg_cut.{args.ndcg_cutoff} beir-v1.0.0-{args.dataset}-test run.beir.{model_map[args.model]}.{args.dataset}.txt",
-        f"python -m pyserini.eval.trec_eval -c -m recall.{args.recall_cutoffs[0]} beir-v1.0.0-{args.dataset}-test run.beir.{model_map[args.model]}.{args.dataset}.txt",
-        f"python -m pyserini.eval.trec_eval -c -m recall.{args.recall_cutoffs[1]} beir-v1.0.0-{args.dataset}-test run.beir.{model_map[args.model]}.{args.dataset}.txt",
+        f"python -m pyserini.eval.trec_eval -c -m ndcg_cut.{args.ndcg_cutoff} beir-v1.0.0-{args.dataset}-test run.beir.{model_map[args.encoder]}.{args.dataset}.txt",
+        f"python -m pyserini.eval.trec_eval -c -m recall.{args.recall_cutoffs[0]} beir-v1.0.0-{args.dataset}-test run.beir.{model_map[args.encoder]}.{args.dataset}.txt",
+        f"python -m pyserini.eval.trec_eval -c -m recall.{args.recall_cutoffs[1]} beir-v1.0.0-{args.dataset}-test run.beir.{model_map[args.encoder]}.{args.dataset}.txt",
     ]
     for command in commands:
         print(
