b0c152fdb16ef9877881cd0ac83beea753095529
jiahong <0701phantom@gmail.com>
2025-09-12 22:16:51 +0800

    Run Document Expansion

--- inference.py
+++ inference.py
@@ -43,5 +43,5 @@ if __name__ == "__main__":
     results = doc2query(corpus)
     DE = {result["id"]: result["querygen"].split("\n") for result in results}
     with open(f"all-t5-base-v1_{args.dataset}_gen{args.num_samples}.json", "w") as f:
-        json.dump(DE, f)
+        json.dump(DE, f, indent=4)
     print("Saved to:", f"all-t5-base-v1_{args.dataset}_gen{args.num_samples}.json")
