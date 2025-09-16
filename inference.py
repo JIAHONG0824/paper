ec4439473fdbe348b22c018030522a8bc5824893
jiahong <0701phantom@gmail.com>
2025-09-16 12:25:43 +0800

    generate query

--- inference.py
+++ inference.py
@@ -6,24 +6,12 @@ import json
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument(
-        "--model",
-        type=str,
-        default="doc2query/all-t5-base-v1",
-    )
-    parser.add_argument(
-        "--dataset",
-        type=str,
-        required=True,
-    )
-    parser.add_argument(
-        "--num_samples",
-        type=int,
-        default=5,
-    )
+    parser.add_argument("--model", type=str, default="doc2query/all-t5-base-v1")
+    parser.add_argument("--dataset", type=str, required=True)
+    parser.add_argument("--num_samples", type=int, default=20)
+    parser.add_argument("--output", type=str, required=True)
     args = parser.parse_args()
     dataset = ir_datasets.load(f"beir/{args.dataset}")
-    print("Corpus size:", dataset.docs_count())
     corpus = []
     for doc in dataset.docs_iter():
         content = getattr(doc, "title", "")
@@ -31,17 +19,16 @@ if __name__ == "__main__":
         if not content:
             continue
         corpus.append({"id": doc.doc_id, "text": content})
-    print("Number of valid documents:", len(corpus))
     doc2query = pyterrier_doc2query.Doc2Query(
         checkpoint=args.model,
         num_samples=args.num_samples,
-        batch_size=40,
+        batch_size=20,
         verbose=True,
         fast_tokenizer=True,
         device="cuda:1",
     )
     results = doc2query(corpus)
     DE = {result["id"]: result["querygen"].split("\n") for result in results}
-    with open(f"all-t5-base-v1_{args.dataset}_gen{args.num_samples}.json", "w") as f:
+    with open(f"{args.output}.json", "w") as f:
         json.dump(DE, f, indent=4)
-    print("Saved to:", f"all-t5-base-v1_{args.dataset}_gen{args.num_samples}.json")
+    print(f"Saved to {args.output}.json")
