0c28e8a7bd663e717b881ffc5bb271c79af26fa7
jiahong <0701phantom@gmail.com>
2026-02-22 13:33:52 +0800

    add --device parameter

--- eval_hc3.py
+++ eval_hc3.py
@@ -1,4 +1,4 @@
-from sentence_transformers import SentenceTransformer
+from sentence_transformers import SentenceTransformer, CrossEncoder
 from pyserini.search.lucene import LuceneSearcher
 from pyserini.search.faiss import FaissSearcher
 from datasets import load_dataset
@@ -7,8 +7,10 @@ import argparse
 import time
 
 prefix = {
-    "facebook/contriever": None,
-    "facebook/contriever-msmarco": None,
+    "intfloat/e5-base-v2": {
+        "query": "query: ",
+        "document": "passage: ",
+    },
     "BAAI/bge-base-en-v1.5": {
         "query": "Represent this sentence for searching relevant passages:",
     },
@@ -17,12 +19,16 @@ prefix = {
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("--model", type=str, default="BM25")
+    parser.add_argument("--device", type=str, required=True)
+    parser.add_argument(
+        "--rerank", action="store_true", help="是否進行第二階段 reranking"
+    )
     args = parser.parse_args()
 
     if args.model != "BM25":
         model = SentenceTransformer(
             args.model,
-            device="cuda:1",
+            device=args.device,
             prompts=prefix[args.model],
             default_prompt_name="query",
         )
@@ -62,3 +68,22 @@ if __name__ == "__main__":
     temp = ir_measures.calc_aggregate([nDCG @ 10, R @ 100], qrels, run)
     for k, v in temp.items():
         print(f"{k}: {v:.4f}")
+    if args.rerank:
+        reranker = CrossEncoder("BAAI/bge-reranker-v2-m3", device=args.device)
+        corpus = load_dataset("mteb/HC3FinanceRetrieval", "corpus")
+        store = {}
+        for item in corpus["test"]:
+            store[item["id"]] = item["text"]
+        h = {}
+        for query_id, text in zip(qids, queries):
+            h[query_id] = text
+        pairs = []
+        for query_id, doc_id, score in run.itertuples(index=False):
+            pairs.append((h[query_id], store[doc_id]))
+        scores = reranker.predict(pairs, show_progress_bar=True, batch_size=64)
+        run["score"] = scores
+        print(run.dtypes)
+        run.sort_values(by=["query_id", "score"], ascending=[True, False], inplace=True)
+        temp = ir_measures.calc_aggregate([nDCG @ 10, R @ 100], qrels, run)
+        for k, v in temp.items():
+            print(f"{k}: {v:.4f}")
